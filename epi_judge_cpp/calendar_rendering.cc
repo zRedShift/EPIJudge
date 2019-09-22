93ec5ea538a1b2fe68bc20e1b522939ad8b0feac
zRedShift <stormrage2007@gmail.com>
2019-09-22 04:25:46 +0300

    sorting (14.3, 14.4)

--- epi_judge_cpp/calendar_rendering.cc
+++ epi_judge_cpp/calendar_rendering.cc
@@ -1,3 +1,4 @@
+#include <variant>
 #include <vector>
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
@@ -6,9 +7,27 @@ using std::vector;
 struct Event {
   int start, finish;
 };
+struct Delimiter {
+  bool operator<(const Delimiter &rhs) const {
+    return value == rhs.value ? !is_end && rhs.is_end : value < rhs.value;
+  }
+  int value;
+  bool is_end;
+};
 int FindMaxSimultaneousEvents(const vector<Event>& A) {
-  // TODO - you fill in here.
-  return 0;
+  vector<Delimiter> vec;
+  for (const auto event: A) {
+    vec.emplace_back(Delimiter{event.start, false});
+    vec.emplace_back(Delimiter{event.finish, true});
+  }
+  std::sort(vec.begin(), vec.end());
+  int simultaneous = 0, simultaneous_max = 0;
+  for (const auto[_, is_end]: vec)
+    if (is_end)
+      simultaneous_max = std::max(simultaneous_max, simultaneous), --simultaneous;
+    else
+      ++simultaneous;
+  return simultaneous_max;
 }
 template <>
 struct SerializationTraits<Event> : UserSerTraits<Event, int, int> {};
--- epi_judge_cpp/remove_duplicates.cc
+++ epi_judge_cpp/remove_duplicates.cc
@@ -8,22 +8,29 @@ using std::string;
 using std::vector;
 
 struct Name {
-  bool operator<(const Name& that) const {
+  bool operator<(const Name &that) const {
     return first_name != that.first_name ? first_name < that.first_name
                                          : last_name < that.last_name;
   }
 
   string first_name, last_name;
 };
-void EliminateDuplicate(vector<Name>* names) {
-  // TODO - you fill in here.
-  return;
+void EliminateDuplicate(vector<Name> *names) {
+  std::unordered_set<string> first_names;
+  int i = 0, j = names->size();
+  while (i < j)
+    if (!first_names.emplace((*names)[i].first_name).second)
+      std::swap((*names)[i], (*names)[--j]);
+    else
+      ++i;
+  names->resize(j);
 }
-template <>
+template<>
 struct SerializationTraits<Name>
-    : UserSerTraits<Name, std::string, std::string> {};
+    : UserSerTraits<Name, std::string, std::string> {
+};
 
-std::ostream& operator<<(std::ostream& out, const Name& n) {
+std::ostream &operator<<(std::ostream &out, const Name &n) {
   return out << n.first_name;
 }
 
@@ -37,10 +44,10 @@ bool Comp(vector<std::string> expected, vector<Name> result) {
   std::sort(begin(result), end(result));
   return std::equal(
       begin(expected), end(expected), begin(result), end(result),
-      [](const std::string& s, const Name& n) { return s == n.first_name; });
+      [](const std::string &s, const Name &n) { return s == n.first_name; });
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"names"};
   return GenericTestMain(args, "remove_duplicates.cc", "remove_duplicates.tsv",

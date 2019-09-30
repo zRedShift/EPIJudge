ee349fb0e9f851744b157a26210542cd684d0d57
zRedShift <stormrage2007@gmail.com>
2019-09-30 09:06:27 +0300

    started dynamic programming

--- epi_judge_cpp/fibonacci.cc
+++ epi_judge_cpp/fibonacci.cc
@@ -1,10 +1,15 @@
 #include "test_framework/generic_test.h"
+
 int Fibonacci(int n) {
-  // TODO - you fill in here.
-  return -1;
+  if (n <= 1)
+    return n;
+  int f_1 = 1, f_2 = 1;
+  for (int i = 2; i < n; ++i)
+    std::swap(f_1 += f_2, f_2);
+  return f_2;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"n"};
   return GenericTestMain(args, "fibonacci.cc", "fibonacci.tsv", &Fibonacci,
--- epi_judge_cpp/max_sum_subarray.cc
+++ epi_judge_cpp/max_sum_subarray.cc
@@ -2,12 +2,19 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
-int FindMaximumSubarray(const vector<int>& A) {
-  // TODO - you fill in here.
-  return -1;
+int FindMaximumSubarray(const vector<int> &A) {
+  int min_sum = 0, max_sum = 0, sum = 0;
+  for (const auto x : A) {
+    sum += x;
+    if (sum < min_sum)
+      min_sum = sum;
+    if (sum - min_sum > max_sum)
+      max_sum = sum - min_sum;
+  }
+  return max_sum;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A"};
   return GenericTestMain(args, "max_sum_subarray.cc", "max_sum_subarray.tsv",

5617b110c3a6642a338520d577876751fce37061
zRedShift <stormrage2007@gmail.com>
2019-10-01 06:06:50 +0300

    dynamic programming (17.1)

--- epi_judge_cpp/number_of_score_combinations.cc
+++ epi_judge_cpp/number_of_score_combinations.cc
@@ -2,13 +2,16 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
-int NumCombinationsForFinalScore(int final_score,
-                                 const vector<int>& individual_play_scores) {
-  // TODO - you fill in here.
-  return 0;
+int NumCombinationsForFinalScore(int final_score, const vector<int> &individual_play_scores) {
+  vector<int> combinations(final_score + 1);
+  combinations.front() = 1;
+  for (const auto x : individual_play_scores)
+    for (int i = 0, j = x; j <= final_score; ++i, ++j)
+      combinations[j] += combinations[i];
+  return combinations.back();
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"final_score", "individual_play_scores"};
   return GenericTestMain(args, "number_of_score_combinations.cc",

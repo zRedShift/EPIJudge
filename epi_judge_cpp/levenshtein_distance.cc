411c7331f90cde94be750633ee774ae4bc6b2d6d
zRedShift <stormrage2007@gmail.com>
2019-10-02 12:27:49 +0300

    dynamic programming (17.2)

--- epi_judge_cpp/levenshtein_distance.cc
+++ epi_judge_cpp/levenshtein_distance.cc
@@ -4,12 +4,22 @@
 using std::string;
 using std::vector;
 
-int LevenshteinDistance(const string& A, const string& B) {
-  // TODO - you fill in here.
-  return 0;
+int LevenshteinDistance(const string &A, const string &B) {
+  auto[small, large] = std::minmax(A, B);
+  vector<int> distance(small.size());
+  std::iota(distance.begin(), distance.end(), 1);
+  for (int i = 0; i < large.size(); ++i) {
+    int substitute = i, insert = i + 1;
+    for (int j = 0; j < small.size(); ++j)
+      if (large[i] == small[j])
+        insert = substitute, substitute = distance[j], distance[j] = insert;
+      else
+        insert = distance[j] = 1 + std::min({substitute, substitute = distance[j], insert});
+  }
+  return distance.back();
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A", "B"};
   return GenericTestMain(args, "levenshtein_distance.cc",

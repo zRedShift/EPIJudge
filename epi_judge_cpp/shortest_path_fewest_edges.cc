43018bad798f29c83de8f96b310377bd8704890d
zRedShift <stormrage2007@gmail.com>
2019-10-16 22:47:19 +0300

    graphs (19.9)

---
+++ epi_judge_cpp/shortest_path_fewest_edges.cc
@@ -0,0 +1,79 @@
+#include <stdexcept>
+#include <vector>
+#include "test_framework/generic_test.h"
+#include "test_framework/serialization_traits.h"
+#include "test_framework/timed_executor.h"
+using std::vector;
+using std::numeric_limits;
+
+struct GraphVertex;
+struct Path {
+  int distance = numeric_limits<int>::max(), length = 0;
+  bool operator<(const Path &rhs) const {
+    return distance < rhs.distance || (distance == rhs.distance && length < rhs.length);
+  }
+};
+struct GraphVertex {
+  vector<std::pair<GraphVertex *, int>> edges;
+  Path path;
+  GraphVertex *pred = nullptr;
+};
+struct VertexComp {
+  bool operator()(const GraphVertex *lhs, const GraphVertex *rhs) const {
+    return lhs->path < rhs->path;
+  }
+};
+
+vector<GraphVertex *> ShortestPathFewestEdges(const GraphVertex *origin, const GraphVertex *destination) {
+  auto curr(const_cast<GraphVertex *>(origin));
+  curr->path = {0, 0};
+  std::set<GraphVertex *, VertexComp> q{curr};
+  do {
+    curr = *q.cbegin();
+    if (curr == destination)
+      break;
+    q.erase(q.cbegin());
+    for (const auto[vertex, distance] : curr->edges)
+      if (Path alt{curr->path.distance + distance, curr->path.length + 1}; alt < vertex->path)
+        q.erase(vertex), vertex->path = alt, vertex->pred = curr, q.insert(vertex);
+  } while (!q.empty());
+  curr = const_cast<GraphVertex *>(destination);
+  vector<GraphVertex *> result(curr->pred ? curr->path.length + 1 : 0);
+  for (auto p = result.rbegin(); p != result.rend(); ++p)
+    *p = curr, curr = curr->pred;
+  return result;
+}
+
+struct Edge {
+  int from, to, distance;
+};
+template<>
+struct SerializationTraits<Edge> : UserSerTraits<Edge, int, int, int> {};
+
+vector<int> ShortestPathFewestEdgesWrapper(TimedExecutor &executor, int k, int from, int to,
+                                           const vector<Edge> &edges) {
+  if (k <= 0) {
+    throw std::runtime_error("Invalid k value");
+  }
+
+  vector<GraphVertex> graph(k, GraphVertex{});
+
+  for (const Edge &e : edges) {
+    if (e.from < 0 || e.from >= k || e.to < 0 || e.to >= k || e.distance <= 0) {
+      throw std::runtime_error("Invalid vertex index");
+    }
+    graph[e.from].edges.emplace_back(&graph[e.to], e.distance);
+  }
+  auto vec = executor.Run([&] { return ShortestPathFewestEdges(&graph[from], &graph[to]); });
+  vector<int> result(vec.size());
+  std::transform(vec.begin(), vec.end(), result.begin(), [&](const auto &v) { return v - graph.data(); });
+  return result;
+}
+
+int main(int argc, char *argv[]) {
+  std::vector<std::string> args{argv + 1, argv + argc};
+  std::vector<std::string> param_names{"executor", "k", "from", "to", "edges"};
+  return GenericTestMain(
+      args, "shortest_path_fewest_edges.cc", "shortest_path_fewest_edges.tsv",
+      &ShortestPathFewestEdgesWrapper, DefaultComparator{}, param_names);
+}

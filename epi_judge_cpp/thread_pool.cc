837fc28031514850185c36c8cc850c0ddc133278
zRedShift <stormrage2007@gmail.com>
2019-10-20 13:03:15 +0300

    parallel computing (20.4)

--- epi_judge_cpp/thread_pool.cc
+++ epi_judge_cpp/thread_pool.cc
@@ -1,35 +1,55 @@
 #include <boost/asio.hpp>
+#include <boost/thread/sync_bounded_queue.hpp>
 #include <iostream>
 
+using std::move;
+using std::unique_ptr;
+using std::ref;
 using std::cout;
 using std::endl;
+using std::thread;
 using boost::system::error_code;
+using boost::sync_bounded_queue;
 namespace asio = boost::asio;
 using asio::ip::tcp;
 
-void ProcessReq(tcp::socket &sock) {
+typedef sync_bounded_queue<unique_ptr<tcp::socket>> QueueType;
+
+void ProcessReq(unique_ptr<tcp::socket> &sock) {
   asio::streambuf sb;
-  while (true) {
+  for (;;) {
     error_code e;
-    size_t n = asio::read_until(sock, sb, '\n', e);
+    size_t n = asio::read_until(*sock, sb, '\n', e);
     if (e == asio::error::eof) {
       cout << endl << "connection closed" << endl;
       break;
     }
-    asio::write(sock, sb.data());
+    asio::write(*sock, sb.data());
     cout << &sb;
   }
 }
 
-const unsigned short SERVERPORT = 8080;
+void ThreadFunc(QueueType &q) {
+  for (;;) {
+    unique_ptr<tcp::socket> sock;
+    q >> sock;
+    ProcessReq(sock);
+  }
+}
+
+const unsigned short kServerPort = 8080;
+const int kNThreads = 23;
 
 int main(int argc, char *argv[]) {
   asio::io_service io_service;
-  tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), SERVERPORT));
-  while (true) {
-    tcp::socket sock(io_service);
-    acceptor.accept(sock);
-    ProcessReq(sock);
+  tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), kServerPort));
+  QueueType q(kNThreads);
+  for (int i = 0; i < kNThreads; ++i)
+    thread(ThreadFunc, ref(q)).detach();
+  for (;;) {
+    unique_ptr<tcp::socket> sock(new tcp::socket(io_service));
+    acceptor.accept(*sock);
+    q << move(sock);
   }
   return 0;
 }

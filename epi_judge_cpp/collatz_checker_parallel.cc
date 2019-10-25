06cf5b8f68eabcc3df74364cd9b756a80ac1dc28
zRedShift <stormrage2007@gmail.com>
2019-10-25 14:40:58 +0300

    use sync bounded queue for parallel collatz checking to balance threads

--- epi_judge_cpp/collatz_checker_parallel.cc
+++ epi_judge_cpp/collatz_checker_parallel.cc
@@ -1,7 +1,7 @@
 #include "test_framework/generic_test.h"
+#include <boost/thread/sync_bounded_queue.hpp>
 
-bool TestCollatzConjecture(int start, int finish) {
-  std::unordered_set<size_t> temp;
+bool TestCollatzConjecture(std::unordered_set<size_t> &temp, int start, int finish) {
   for (size_t i = start; i <= finish; i += 2) {
     size_t k = i;
     do {
@@ -17,22 +17,38 @@ bool TestCollatzConjecture(int start, int finish) {
   return true;
 }
 
+bool Executor(boost::sync_bounded_queue<std::pair<int, int>> &q) {
+  std::pair<int, int> args;
+  std::unordered_set<size_t> temp;
+  while (q.wait_pull_front(args) != boost::queue_op_status::closed)
+    if (!TestCollatzConjecture(temp, args.first, args.second))
+      return false;
+  return true;
+}
+
 bool TestCollatzConjectureWrapper(int n) {
-  static const int num_threads = std::max(std::thread::hardware_concurrency(), 2u);
-  const int adder = (n - 3) / num_threads;
-  if (adder < num_threads)
-    return TestCollatzConjecture(3, n);
+  const int kRange = 100'000;
+  if (n < 3)
+    return true;
+  if (n <= kRange)
+    return TestCollatzConjecture(*std::make_unique<std::unordered_set<size_t>>().get(), 3, n);
+  static const size_t kNumThreads = std::max(std::thread::hardware_concurrency(), 2u);
+  boost::sync_bounded_queue<std::pair<int, int>> q{kNumThreads};
   std::vector<std::future<bool>> futures;
-  futures.reserve(num_threads);
-  for (int start = 3, finish = start + adder + (n - 3) % num_threads; start < n; start = finish, finish += adder)
-    futures.emplace_back(std::async(std::launch::async, TestCollatzConjecture, start, finish));
+  futures.reserve(kNumThreads);
+  for (int i = 0; i < kNumThreads; ++i)
+    futures.emplace_back(std::async(std::launch::async, Executor, std::ref(q)));
+  q << std::make_pair(3, kRange + n % kRange);
+  for (int i = kRange + n % kRange; i < n; i += kRange)
+    q << std::make_pair(i, i + kRange);
+  q.close();
   return std::all_of(futures.begin(), futures.end(), [](auto &f) { return f.get(); });
 }
 
 int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"n"};
-  return GenericTestMain(args, "collatz_checker.cc", "collatz_checker.tsv",
+  return GenericTestMain(args, "collatz_checker_parallel.cc", "collatz_checker_parallel.tsv",
                          &TestCollatzConjectureWrapper, DefaultComparator{},
                          param_names);
 }

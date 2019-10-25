f95ae92288cf6a9659d4dc95391d12a6bd7b452c
zRedShift <ronen.ulanovsky.5@gmail.com>
2019-10-26 01:57:30 +0300

    arrays (6.4, 6.7)

--- epi_judge_cpp/advance_by_offsets.cc
+++ epi_judge_cpp/advance_by_offsets.cc
@@ -1,12 +1,14 @@
 #include <vector>
 #include "test_framework/generic_test.h"
 using std::vector;
-bool CanReachEnd(const vector<int>& max_advance_steps) {
-  // TODO - you fill in here.
-  return true;
+bool CanReachEnd(const vector<int> &max_advance_steps) {
+  size_t j = 0, last = max_advance_steps.size() - 1;
+  for (size_t i = 0; i <= j && j < last; ++i)
+    j = std::max(j, i + max_advance_steps[i]);
+  return j >= last;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"max_advance_steps"};
   return GenericTestMain(args, "advance_by_offsets.cc",
--- epi_judge_cpp/buy_and_sell_stock_twice.cc
+++ epi_judge_cpp/buy_and_sell_stock_twice.cc
@@ -1,12 +1,35 @@
 #include <vector>
 #include "test_framework/generic_test.h"
 using std::vector;
-double BuyAndSellStockTwice(const vector<double>& prices) {
-  // TODO - you fill in here.
-  return 0.0;
+double BuyAndSellStockTwiceAlternative(const vector<double> &prices) {
+  double ext = prices.front(), prev = 0;
+  vector<double> best_trades(prices.size());
+  for (int i = 1; i < prices.size(); ++i) {
+    prev = best_trades[i] = std::max(prev, prices[i] - ext);
+    ext = std::min(ext, prices[i]);
+  }
+  ext = prices.back(), prev = 0;
+  double best = best_trades.back();
+  for (int j = prices.size() - 2; j > -1; --j) {
+    prev = std::max(prev, ext - prices[j]);
+    best = std::max(best, best_trades[j] + prev);
+    ext = std::max(ext, prices[j]);
+  }
+  return best;
 }
 
-int main(int argc, char* argv[]) {
+double BuyAndSellStockTwice(const vector<double> &prices) {
+  std::pair<double, double> min{std::numeric_limits<double>::max(), std::numeric_limits<double>::max()}, best{0.0, 0.0};
+  for (const auto price : prices) {
+    best.first = std::max(best.first, price - min.first);
+    best.second = std::max(best.second, price - min.second);
+    min.first = std::min(min.first, price);
+    min.second = std::min(min.second, price - best.first);
+  }
+  return best.second;
+}
+
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"prices"};
   return GenericTestMain(args, "buy_and_sell_stock_twice.cc",

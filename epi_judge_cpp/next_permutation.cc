30713f7dd1fd3a3baff411a9d291e93472765048
zRedShift <ronen.ulanovsky.5@gmail.com>
2019-10-28 23:58:09 +0200

    arrays (6.10, 6.12)

--- epi_judge_cpp/next_permutation.cc
+++ epi_judge_cpp/next_permutation.cc
@@ -2,11 +2,15 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 vector<int> NextPermutation(vector<int> perm) {
-  // TODO - you fill in here.
-  return {};
+  auto p = std::is_sorted_until(perm.rbegin(), perm.rend());
+  if (p == perm.rend())
+    return {};
+  std::iter_swap(p, std::upper_bound(perm.rbegin(), p, *p));
+  std::reverse(perm.rbegin(), p);
+  return perm;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"perm"};
   return GenericTestMain(args, "next_permutation.cc", "next_permutation.tsv",
--- epi_judge_cpp/online_sampling.cc
+++ epi_judge_cpp/online_sampling.cc
@@ -2,20 +2,29 @@
 #include <functional>
 #include <iterator>
 #include <vector>
+#include <random>
 #include "test_framework/generic_test.h"
 #include "test_framework/random_sequence_checker.h"
 #include "test_framework/timed_executor.h"
 using std::bind;
-using std::sort;
 using std::vector;
+using std::default_random_engine;
+using std::uniform_int_distribution;
+using std::uniform_real_distribution;
+using std::swap;
+using std::random_device;
+using std::random_shuffle;
+
 // Assumption: there are at least k elements in the stream.
 vector<int> OnlineRandomSample(vector<int>::const_iterator stream_begin,
                                const vector<int>::const_iterator stream_end,
-                               int k) {
-  // TODO - you fill in here.
-  return {};
+                               const int k) {
+  default_random_engine re((random_device()) ());
+  vector<int> result(stream_begin, stream_end);
+  shuffle(result.begin(), result.end(), re);
+  return {result.begin(), result.begin() + k};
 }
-bool OnlineRandomSamplingRunner(TimedExecutor& executor, vector<int> stream,
+bool OnlineRandomSamplingRunner(TimedExecutor &executor, vector<int> stream,
                                 int k) {
   vector<vector<int>> results;
 
@@ -43,13 +52,13 @@ bool OnlineRandomSamplingRunner(TimedExecutor& executor, vector<int> stream,
                                         0.01);
 }
 
-void OnlineRandomSampleWrapper(TimedExecutor& executor,
-                               const vector<int>& stream, int k) {
+void OnlineRandomSampleWrapper(TimedExecutor &executor,
+                               const vector<int> &stream, int k) {
   RunFuncWithRetries(bind(OnlineRandomSamplingRunner, std::ref(executor),
                           std::cref(stream), k));
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "stream", "k"};
   return GenericTestMain(args, "online_sampling.cc", "online_sampling.tsv",

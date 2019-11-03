b70af6999a51677cad065083c0b2b8d3b9c496f0
zRedShift <ronen.ulanovsky.5@gmail.com>
2019-11-03 23:47:55 +0200

    25.1, 25.6

--- epi_judge_cpp/gcd.cc
+++ epi_judge_cpp/gcd.cc
@@ -1,11 +1,28 @@
 #include "test_framework/generic_test.h"
 
-long long Gcd(long long x, long long y) {
-  // TODO - you fill in here.
-  return 0;
+size_t Gcd(size_t x, size_t y) {
+  if (!x || !y)
+    return std::max(x, y);
+  size_t shift = 0;
+  while (!((x | y) & 1u))
+    x >>= 1u, y >>= 1u, ++shift;
+  while (!(x & 1u))
+    x >>= 1u;
+  while (!(y & 1u))
+    y >>= 1u;
+  if (x > y)
+    std::swap(x, y);
+  while (x != y && x != 1) {
+    size_t temp = y - x;
+    do {
+      temp >>= 1u;
+    } while (!(temp & 1u));
+    std::tie(x, y) = std::minmax({x, temp});
+  }
+  return x << shift;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x", "y"};
   return GenericTestMain(args, "gcd.cc", "gcd.tsv", &Gcd, DefaultComparator{},
--- epi_judge_cpp/rotate_array.cc
+++ epi_judge_cpp/rotate_array.cc
@@ -3,17 +3,24 @@
 #include "test_framework/timed_executor.h"
 using std::vector;
 
-void RotateArray(int rotate_amount, vector<int>* A) {
-  // TODO - you fill in here.
-  return;
+void RotateArray(int rotate_amount, vector<int> *A_ptr) {
+  auto &A = *A_ptr;
+  int actual_amount = rotate_amount % A.size(), gcd = std::gcd(actual_amount, A.size());
+  if (!actual_amount)
+    return;
+  for (int i = 0, num = actual_amount; i < gcd; ++i, ++num)
+    do {
+      for (; num < A.size(); num += actual_amount)
+        std::swap(A[i], A[num]);
+    } while ((num %= A.size()) != i);
 }
-vector<int> RotateArrayWrapper(TimedExecutor& executor, vector<int> A,
+vector<int> RotateArrayWrapper(TimedExecutor &executor, vector<int> A,
                                int rotate_amount) {
   executor.Run([&] { RotateArray(rotate_amount, &A); });
   return A;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "A", "rotate_amount"};
   return GenericTestMain(args, "rotate_array.cc", "rotate_array.tsv",

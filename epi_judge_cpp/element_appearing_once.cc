e9ec34ae3f784b62681cc6c951cbfa7d2eae1216
zRedShift <ronen.ulanovsky.5@gmail.com>
2019-11-05 23:50:43 +0200

    25.18

--- epi_judge_cpp/element_appearing_once.cc
+++ epi_judge_cpp/element_appearing_once.cc
@@ -1,13 +1,20 @@
 #include <vector>
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::array;
 
-int FindElementAppearsOnce(const vector<int>& A) {
-  // TODO - you fill in here.
-  return 0;
+int FindElementAppearsOnce(const vector<int> &A) {
+  array<unsigned, 8 * sizeof(int)> hist{};
+  for (unsigned x : A)
+    for (int i = 0; x; x >>= 1u)
+      hist[i++] += x & 1u;
+  unsigned result = 0;
+  for (unsigned i = 0; i < hist.size(); ++i)
+    result |= (hist[i] % 3u) << i;
+  return static_cast<int>(result);
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A"};
   return GenericTestMain(args, "element_appearing_once.cc",

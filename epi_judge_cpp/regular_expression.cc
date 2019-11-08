214ce71c7240621bce432f281380063c34c089c1
zRedShift <ronen.ulanovsky.5@gmail.com>
2019-11-08 20:06:00 +0200

    25.26

--- epi_judge_cpp/regular_expression.cc
+++ epi_judge_cpp/regular_expression.cc
@@ -2,9 +2,41 @@
 #include "test_framework/generic_test.h"
 using std::string;
 
+bool IsMatchHelper(const char *regex, const char *s) {
+  const char r = *regex++, c = *s;
+  switch (r) {
+    case 0:return true;
+    case '$':return !c;
+    case '.':
+      if (*regex != '*')
+        return c && IsMatchHelper(regex, s + 1);
+      ++regex;
+      do {
+        if (IsMatchHelper(regex, s))
+          return true;
+      } while (*s++);
+      return false;
+    default:
+      if (*regex != '*')
+        return r == c && IsMatchHelper(regex, s + 1);
+      ++regex;
+      do {
+        if (IsMatchHelper(regex, s))
+          return true;
+      } while (*s++ == r);
+      return false;
+  }
+}
+
 bool IsMatch(const string &regex, const string &s) {
-  // TODO - you fill in here.
-  return true;
+  if (regex.empty())
+    return true;
+  if (regex.front() == '^')
+    return IsMatchHelper(regex.c_str() + 1, s.c_str());
+  for (auto iter = s.c_str(); *iter; ++iter)
+    if (IsMatchHelper(regex.c_str(), iter))
+      return true;
+  return IsMatchHelper(regex.c_str(), "");
 }
 
 int main(int argc, char *argv[]) {

293e5cc4a7619213f0a6a250bf49d054cd157fff
zRedShift <ronen.ulanovsky.5@gmail.com>
2019-11-09 01:29:31 +0200

    25.33

--- epi_judge_cpp/max_square_submatrix.cc
+++ epi_judge_cpp/max_square_submatrix.cc
@@ -4,12 +4,19 @@
 using std::deque;
 using std::vector;
 
-int MaxSquareSubmatrix(const vector<deque<bool>>& A) {
-  // TODO - you fill in here.
-  return 0;
+int MaxSquareSubmatrix(const vector<deque<bool>> &A) {
+  vector<int> cache(A.front().size()), curr;
+  int result = 0;
+  for (const auto &row : A) {
+    curr.assign(row.cbegin(), row.cend());
+    for (int i = 1; i < cache.size(); ++i)
+      curr[i] *= 1 + std::min({cache[i], cache[i - 1], curr[i - 1]});
+    std::swap(curr, cache), result = std::max(result, *std::max_element(cache.begin(), cache.end()));
+  }
+  return result * result;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A"};
   return GenericTestMain(args, "max_square_submatrix.cc",
--- epi_judge_cpp/max_submatrix.cc
+++ epi_judge_cpp/max_submatrix.cc
@@ -4,12 +4,23 @@
 using std::deque;
 using std::vector;
 
-int MaxRectangleSubmatrix(const vector<deque<bool>>& A) {
-  // TODO - you fill in here.
-  return 0;
+int MaxRectangleSubmatrix(const vector<deque<bool>> &A) {
+  vector<int> cache(A.front().size()), stack;
+  int result = 0, height;
+  for (const auto &row : A) {
+    for (int i = 0; i < cache.size(); stack.push_back(i++)) {
+      int curr = ++cache[i] *= row[i];
+      while (!stack.empty() && (height = cache[stack.back()]) >= curr)
+        stack.pop_back(), result = std::max(result, height * (i - (stack.empty() ? 0 : stack.back() + 1)));
+    }
+    while (!stack.empty())
+      height = cache[stack.back()], stack.pop_back(), result =
+          std::max(result, height * static_cast<int>(cache.size() - (stack.empty() ? 0 : stack.back() + 1)));
+  }
+  return result;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A"};
   return GenericTestMain(args, "max_submatrix.cc", "max_submatrix.tsv",

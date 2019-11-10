f0104b9b0e9418206caa4fd47fe1460a39f85328
zRedShift <ronen.ulanovsky.5@gmail.com>
2019-11-10 23:59:14 +0200

    25.17

--- epi_judge_cpp/kth_largest_element_in_long_array.cc
+++ epi_judge_cpp/kth_largest_element_in_long_array.cc
@@ -3,16 +3,23 @@
 using std::vector;
 
 int FindKthLargestUnknownLength(vector<int>::const_iterator stream_begin,
-                                const vector<int>::const_iterator& stream_end,
+                                const vector<int>::const_iterator &stream_end,
                                 int k) {
-  // TODO - you fill in here.
-  return 0;
+  auto curr = std::min(stream_begin + 2 * k, stream_end);
+  vector<int> A(stream_begin, curr);
+  auto result = A.end() - k;
+  std::nth_element(A.begin(), result, A.end());
+  for (; curr + k <= stream_end; curr += k)
+    std::copy_n(curr, k, A.begin()), std::nth_element(A.begin(), result, A.end());
+  if (curr != stream_end)
+    std::copy(curr, stream_end, A.begin()), std::nth_element(A.begin(), result, A.end());
+  return *result;
 }
-int FindKthLargestUnknownLengthWrapper(const vector<int>& stream, int k) {
+int FindKthLargestUnknownLengthWrapper(const vector<int> &stream, int k) {
   return FindKthLargestUnknownLength(stream.cbegin(), stream.cend(), k);
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"stream", "k"};
   return GenericTestMain(args, "kth_largest_element_in_long_array.cc",

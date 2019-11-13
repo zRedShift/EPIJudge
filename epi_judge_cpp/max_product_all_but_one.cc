a4cddb576b55d84cff318708dbfa4a383a7a1167
zRedShift <ronen.ulanovsky.5@gmail.com>
2019-11-14 01:43:17 +0200

    25.4

--- epi_judge_cpp/max_product_all_but_one.cc
+++ epi_judge_cpp/max_product_all_but_one.cc
@@ -2,12 +2,16 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
-int FindBiggestNMinusOneProduct(const vector<int>& A) {
-  // TODO - you fill in here.
-  return 0;
+int FindBiggestNMinusOneProduct(const vector<int> &A) {
+  vector<int> B(A.size() - 1);
+  std::partial_sum(A.cbegin(), A.cend() - 1, B.begin(), std::multiplies<>());
+  int max = B.back(), x = A.back();
+  for (int i = B.size() - 2; i > -1; --i)
+    max = std::max(max, B[i] * x), x *= A[i + 1];
+  return std::max(max, x);
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A"};
   return GenericTestMain(

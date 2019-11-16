51a70760e93b37896d570d1437d55a78f205c1db
zRedShift <ronen.ulanovsky.5@gmail.com>
2019-11-16 22:48:20 +0200

    25.38

--- epi_judge_cpp/longest_subarray_with_sum_constraint.cc
+++ epi_judge_cpp/longest_subarray_with_sum_constraint.cc
@@ -2,12 +2,27 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
-int FindLongestSubarrayLessEqualK(const vector<int>& A, int k) {
-  // TODO - you fill in here.
-  return 0;
+int FindLongestSubarrayLessEqualK(const vector<int> &A, int k) {
+  vector<std::pair<int, int>> max{{0, 0}}, rmax{{A.size(), 0}};
+  int sum = 0, rsum = 0, length = 0;
+  for (int i = 0, j = A.size() - 1; j > -1; ++i, --j) {
+    sum += A[i], rsum += A[j];
+    if (sum > max.back().second)
+      max.emplace_back(i + 1, sum);
+    if (rsum > rmax.back().second)
+      rmax.emplace_back(j, rsum);
+  }
+  if (sum <= k)
+    return A.size();
+  for (int i = 0, j = rmax.size() - 1; i < max.size() && j > -1;)
+    if (sum - rmax[j].second - max[i].second > k)
+      ++i;
+    else
+      length = std::max(length, rmax[j].first - max[i].first), --j;
+  return length;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A", "k"};
   return GenericTestMain(args, "longest_subarray_with_sum_constraint.cc",

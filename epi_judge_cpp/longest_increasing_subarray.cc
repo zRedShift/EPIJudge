489d39abac97465857a691e2d4e80cbbf8dc4f3c
zRedShift <ronen.ulanovsky.5@gmail.com>
2019-11-17 23:55:37 +0200

    25.5 trivial solution

--- epi_judge_cpp/longest_increasing_subarray.cc
+++ epi_judge_cpp/longest_increasing_subarray.cc
@@ -6,16 +6,26 @@ struct Subarray {
   int start = 0, end = 0;
 };
 
-Subarray FindLongestIncreasingSubarray(const vector<int>& A) {
-  // TODO - you fill in here.
-  return {};
+Subarray FindLongestIncreasingSubarray(const vector<int> &A) {
+  int start = 0, len = 1, max_len = 0;
+  for (int i = 1; i < A.size(); ++i) {
+    if (A[i] <= A[i - 1]) {
+      if (len > max_len)
+        start = i - len, max_len = len;
+      len = 1;
+    } else
+      ++len;
+  }
+  if (len > max_len)
+    start = A.size() - len, max_len = len;
+  return {start, start + max_len - 1};
 }
-int FindLongestIncreasingSubarrayWrapper(const vector<int>& A) {
+int FindLongestIncreasingSubarrayWrapper(const vector<int> &A) {
   Subarray result = FindLongestIncreasingSubarray(A);
   return result.end - result.start + 1;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A"};
   return GenericTestMain(

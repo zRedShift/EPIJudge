66017680665a390ec696505c5e26384e12dfc4d3
zRedShift <ronen.ulanovsky.5@gmail.com>
2019-11-18 01:22:03 +0200

    25.5 optimized

--- epi_judge_cpp/longest_increasing_subarray.cc
+++ epi_judge_cpp/longest_increasing_subarray.cc
@@ -7,6 +7,33 @@ struct Subarray {
 };
 
 Subarray FindLongestIncreasingSubarray(const vector<int> &A) {
+  int start = 0, max_len = 1, i = 1, j, k;
+  while (i < A.size()) {
+    while (i < A.size() && A[i] > A[i - 1])
+      ++i, ++max_len;
+    if (start = i - max_len, j = i, i += max_len; i >= A.size())
+      break;
+    while (A[i] > A[i - 1])
+      --i;
+    if (i == j) {
+      i += max_len += 1;
+      continue;
+    }
+    i += max_len, k = i, j += max_len;
+    while (i < A.size()) {
+      while (A[i] > A[i - 1] && i != j)
+        --i;
+      if (i == j) {
+        i = k + 1, max_len += 1;
+        break;
+      }
+      i += max_len, j = k, k = i;
+    }
+  }
+  return {start, start + max_len - 1};
+}
+
+Subarray FindLongestIncreasingSubarrayAlt(const vector<int> &A) {
   int start = 0, len = 1, max_len = 0;
   for (int i = 1; i < A.size(); ++i) {
     if (A[i] <= A[i - 1]) {
@@ -20,6 +47,7 @@ Subarray FindLongestIncreasingSubarray(const vector<int> &A) {
     start = A.size() - len, max_len = len;
   return {start, start + max_len - 1};
 }
+
 int FindLongestIncreasingSubarrayWrapper(const vector<int> &A) {
   Subarray result = FindLongestIncreasingSubarray(A);
   return result.end - result.start + 1;

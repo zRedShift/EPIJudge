4bfffd0a086c94349a1f0e939556c24a28d9a569
zRedShift <ronen.ulanovsky.5@gmail.com>
2019-11-19 00:54:18 +0200

    25.40

--- epi_judge_cpp/arbitrage.cc
+++ epi_judge_cpp/arbitrage.cc
@@ -3,11 +3,21 @@
 using std::vector;
 
 bool IsArbitrageExist(vector<vector<double>> graph) {
-  // TODO - you fill in here.
-  return true;
+  for (auto &row: graph)
+    for (auto &x : row)
+      x = log2(x);
+  bool changed;
+  do {
+    changed = false;
+    for (int i = 1; i < graph.size(); ++i)
+      for (int j = 0; j < graph.size(); ++j)
+        if (graph[0][i] + graph[i][j] > graph[0][j])
+          graph[0][j] = graph[0][i] + graph[i][j], changed = true;
+  } while (!graph[0][0] && changed);
+  return changed;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"graph"};
   return GenericTestMain(args, "arbitrage.cc", "arbitrage.tsv",

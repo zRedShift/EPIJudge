0c76f301428e9b86256757c630fe4d2d1fce4d6e
zRedShift <ronen.ulanovsky.5@gmail.com>
2019-11-18 17:33:46 +0200

    25.2

--- epi_judge_cpp/first_missing_positive_entry.cc
+++ epi_judge_cpp/first_missing_positive_entry.cc
@@ -2,13 +2,17 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
-// A is passed by value argument, since we change it.
-int FindFirstMissingPositive(vector<int> A) {
-  // TODO - you fill in here.
-  return 0;
+int FindFirstMissingPositive(vector<int> &A) {
+  for (int i = 0; i < A.size(); ++i)
+    while (A[i] > 0 && A[i] <= A.size() && A[i] != A[A[i] - 1])
+      std::swap(A[i], A[A[i] - 1]);
+  for (int i = 0; i < A.size(); ++i)
+    if (A[i] != i + 1)
+      return i + 1;
+  return A.size() + 1;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A"};
   return GenericTestMain(args, "first_missing_positive_entry.cc",

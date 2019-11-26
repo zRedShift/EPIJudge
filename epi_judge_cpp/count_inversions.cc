5ea0eac4db7e8d5a34d12c1ab9541a7956040e1f
zRedShift <ronen.ulanovsky.5@gmail.com>
2019-11-26 23:56:27 +0200

    25.28 brute force

--- epi_judge_cpp/count_inversions.cc
+++ epi_judge_cpp/count_inversions.cc
@@ -2,12 +2,15 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
-int CountInversions(vector<int> A) {
-  // TODO - you fill in here.
-  return 0;
+int CountInversions(const vector<int> &A) {
+  int count = 0;
+  for (int i = 0; i < A.size(); ++i)
+    for (int j = i + 1; j < A.size(); ++j)
+      count += A[i] > A[j];
+  return count;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"A"};
   return GenericTestMain(args, "count_inversions.cc", "count_inversions.tsv",
